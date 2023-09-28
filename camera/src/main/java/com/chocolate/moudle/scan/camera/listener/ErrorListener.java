0a79924753ee650c51471763c61da372c0a4f42a
wang xu <wang867103701@gmail.com>
2023-09-28 22:39:22 +0800

    [mod]: 添加逻辑代码.

---
+++ camera/src/main/java/com/chocolate/moudle/scan/camera/listener/ErrorListener.java
@@ -0,0 +1,10 @@
+package com.chocolate.moudle.scan.camera.listener;
+
+/**
+ * author hbzhou
+ * date 2019/12/13 10:49
+ */
+public interface ErrorListener {
+    void onError();
+    void AudioPermissionError();
+}
---
+++ camera/src/main/java/com/chocolate/moudle/scan/camera/listener/ReturnListener.java
@@ -0,0 +1,9 @@
+package com.chocolate.moudle.scan.camera.listener;
+
+/**
+ * author hbzhou
+ * date 2019/12/13 10:49
+ */
+public interface ReturnListener {
+    void onReturn();
+}
