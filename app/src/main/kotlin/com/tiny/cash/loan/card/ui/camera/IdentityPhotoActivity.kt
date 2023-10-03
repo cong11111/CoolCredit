2b1be664a1c824627e00b9a4378ca8a1cefac124
wangxu <wangxu@drojian.dev>
2023-10-03 20:19:10 +0800

    [mod]:bug fix.

--- app/src/main/kotlin/com/tiny/cash/loan/card/ui/camera/IdentityPhotoActivity.kt
+++ app/src/main/kotlin/com/tiny/cash/loan/card/ui/camera/IdentityPhotoActivity.kt
@@ -3,10 +3,15 @@ package com.tiny.cash.loan.card.ui.camera
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.FrameLayout
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
+import com.blankj.utilcode.util.BarUtils
+import com.bumptech.glide.Glide
+import com.chocolate.moudle.scan.camera2.CameraActivity2
 import com.tiny.cash.loan.card.kudicredit.R
 import com.tiny.cash.loan.card.ui.base.BaseActivity2
 
@@ -14,6 +19,9 @@ class IdentityPhotoActivity : BaseActivity2() {
 
     companion object {
         private const val TAG = "IdentityPhotoActivity"
+
+        const val TYPE_MIN = 101
+        const val TYPE_VOTER_CARD = 102
 
         fun launchActivity(context: Activity) {
             var intent = Intent(context, IdentityPhotoActivity::class.java)
@@ -23,36 +31,86 @@ class IdentityPhotoActivity : BaseActivity2() {
     }
 
     private var isFront : Boolean = true
+    private var mType : Int = TYPE_MIN
 
     private var ivBack : AppCompatImageView? = null
     private var tvNin : AppCompatTextView? = null
     private var tvVotorCard : AppCompatTextView? = null
+    private var ivCenter : AppCompatImageView? = null
+    private var flTap : FrameLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BarUtils.setStatusBarLightMode(this, true)
         setContentView(R.layout.activity_identity_photo)
         ivBack = findViewById<AppCompatImageView>(R.id.iv_identity_photo_back)
         tvNin = findViewById<AppCompatTextView>(R.id.tv_identity_photo_nin)
         tvVotorCard = findViewById<AppCompatTextView>(R.id.tv_identity_photo_voter_card)
+        ivCenter = findViewById<AppCompatImageView>(R.id.iv_center)
+        flTap = findViewById<FrameLayout>(R.id.fl_identity_tap)
         tvNin?.setOnClickListener(object : OnClickListener {
             override fun onClick(v: View?) {
                 isFront = true
+                mType = TYPE_MIN
                 updateNinAndVotorCard()
+                updateMinAndVoterCardPreview()
             }
 
         })
         tvVotorCard?.setOnClickListener(object : OnClickListener {
             override fun onClick(v: View?) {
                 isFront = false
+                mType = TYPE_VOTER_CARD
                 updateNinAndVotorCard()
+                updateMinAndVoterCardPreview()
+            }
+
+        })
+        flTap?.setOnClickListener(object : OnClickListener {
+            override fun onClick(v: View?) {
+                CameraActivity2.showMe(this@IdentityPhotoActivity, mType)
             }
 
         })
         updateNinAndVotorCard()
+        updateMinAndVoterCardPreview()
     }
+
+    private var minPath : String? = null
+    private var votorCardPath : String? = null
 
     private fun updateNinAndVotorCard() {
         tvNin?.isSelected = isFront
         tvVotorCard?.isSelected = !isFront
+    }
+
+    private fun updateMinAndVoterCardPreview() {
+        if (mType ==  TYPE_MIN) {
+            if (TextUtils.isEmpty(minPath)){
+                ivCenter?.setImageResource(R.drawable.identity_1)
+            } else {
+                Glide.with(this).load(minPath).into(ivCenter!!)
+            }
+        } else {
+            if (TextUtils.isEmpty(votorCardPath)){
+                ivCenter?.setImageResource(R.drawable.identity_bvn_2)
+            } else {
+                Glide.with(this).load(votorCardPath).into(ivCenter!!)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CameraActivity2.RESULT_CAMERA_CODE) {
+            val path = data?.getStringExtra(CameraActivity2.KEY_RESULT_CAMERA_PATH)
+            val type = data?.getIntExtra(CameraActivity2.KEY_RESULT_CAMERA_TYPE, TYPE_MIN)
+            if (type == TYPE_MIN) {
+                minPath = path
+            } else if (type == TYPE_VOTER_CARD) {
+                votorCardPath = path
+            }
+            updateMinAndVoterCardPreview()
+        }
     }
 }
--- camera/src/main/java/com/chocolate/moudle/scan/camera2/CameraActivity2.kt
+++ camera/src/main/java/com/chocolate/moudle/scan/camera2/CameraActivity2.kt
@@ -6,36 +6,47 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.blankj.utilcode.util.BarUtils
+import com.blankj.utilcode.util.LogUtils.TYPE
 import com.chocolate.moudle.scan.R
 import com.chocolate.moudle.scan.my.UploadFileFragment
 
 class CameraActivity2 : AppCompatActivity() {
 
     companion object {
+        const val TYPE_MIN = 101
+        const val TYPE_VOTER_CARD = 102
 
-        fun showMe(activity: Activity) {
+        const val RESULT_CAMERA_CODE = 1112
+        const val KEY_RESULT_CAMERA_PATH = "key_result_camera_path"
+        const val KEY_RESULT_CAMERA_TYPE = "key_result_camera_type"
+
+        fun showMe(activity: Activity, type : Int) {
             val intent = Intent(activity, CameraActivity2::class.java)
-            activity.startActivity(intent)
+            intent.putExtra(KEY_RESULT_CAMERA_TYPE, type)
+            activity.startActivityForResult(intent, RESULT_CAMERA_CODE)
 //            activity.overridePendingTransition(R.anim.td_slide_in_right, R.anim.td_slide_out_left)
         }
 
     }
 
+    private var mType : Int = TYPE_MIN
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BarUtils.setStatusBarColor(this@CameraActivity2, android.graphics.Color.TRANSPARENT)
         setContentView(R.layout.activity_camera2)
+        mType = intent.getIntExtra(KEY_RESULT_CAMERA_TYPE, TYPE_MIN)
         toCameraFragment()
     }
 
-    private fun toCameraFragment() {
+    fun toCameraFragment(needAnim: Boolean = true) {
         var fragment = supportFragmentManager.findFragmentByTag(Camera2Fragment.TAG)
         if (fragment == null) {
             fragment = Camera2Fragment()
         }
         val bundle = Bundle()
         fragment.arguments = bundle
-        toFragment(fragment, Camera2Fragment.TAG)
+        toFragment(fragment, Camera2Fragment.TAG, needAnim)
     }
 
     private var mPath : String? = null
@@ -52,19 +63,39 @@ class CameraActivity2 : AppCompatActivity() {
         toFragment(fragment, UploadFileFragment.TAG)
     }
 
+    fun toCameraResultFragment(){
+        var fragment = supportFragmentManager.findFragmentByTag(CameraResultFragment.TAG)
+        if (fragment == null) {
+            fragment = CameraResultFragment()
+        }
+        val bundle = Bundle()
+        fragment.arguments = bundle
+        toFragment(fragment, CameraResultFragment.TAG)
+    }
+
     fun getPath() : String? {
         return mPath
     }
 
-    private fun toFragment(fragment: Fragment, tag: String) {
+    fun finishResult() {
+        val intent = Intent()
+        intent.putExtra(KEY_RESULT_CAMERA_PATH, mPath)
+        intent.putExtra(KEY_RESULT_CAMERA_TYPE, mType)
+        setResult(RESULT_CAMERA_CODE, intent)
+        finish()
+    }
+
+    private fun toFragment(fragment: Fragment, tag: String, needAnim : Boolean = true) {
         val fragmentManager = supportFragmentManager
         val transaction = fragmentManager.beginTransaction() // 开启一个事务
-        transaction.setCustomAnimations(
-            R.anim.td_slide_in_right,
-            R.anim.td_slide_out_left,
-            R.anim.td_slide_in_left,
-            R.anim.td_slide_out_right
-        )
+        if (needAnim) {
+            transaction.setCustomAnimations(
+                R.anim.td_slide_in_right,
+                R.anim.td_slide_out_left,
+                R.anim.td_slide_in_left,
+                R.anim.td_slide_out_right
+            )
+        }
         transaction.replace(R.id.fl_camera_2, fragment, tag)
         transaction.commitAllowingStateLoss()
     }
